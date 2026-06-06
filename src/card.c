9ef1ff2c0ee68d4230c36c85074b5ba0fe9e0903
CaioSwe <caio.sweiver.carvalho@uel.br>
2026-06-06 14:59:21 -0300

    card.c | main.c: Código modulado
     - Versão primária com cartas e animações básicas de pegar, largar e mover cartas.

--- src/card.c
+++ src/card.c
@@ -1,5 +1,11 @@
 #include "card.h"
 
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#include <stdio.h>
+
 #include <animation.h>
 
 typedef struct CardStr {
@@ -7,13 +13,192 @@ typedef struct CardStr {
     Animation anim;
 
     Rectangle rect;
-    Rectangle rectOriginal;
-    
+
+    float scaleRatio;
+
     bool hovered;
+    bool grabbed;
     bool attached;
 
     float rotation;
 
+    bool isMaximized;
+    float time;
+
     Color cor;
 } CardStr;
 
+Card Card_Init(Rectangle rect, Texture2D txr){
+    CardStr* card = (CardStr*)malloc(sizeof(CardStr));
+    
+    card->img = txr;
+
+    card->anim = Animation_Init();
+    Animation_AddPositionAnimation(card->anim, easeOutBack);
+    Animation_AddScaleAnimation(card->anim, rect, easeOutBack);
+
+    card->rect = rect;
+    card->scaleRatio = 1.0f;
+
+    card->hovered = false;
+    card->grabbed = false;
+    card->attached = false;
+    
+    card->rotation = 0.0f;
+
+    card->cor = WHITE;
+
+    card->isMaximized = true;
+    card->time = 0.0f;
+
+    return card;
+}
+
+Rectangle Card_GetRect(Card card){
+    return ((CardStr*)card)->rect;
+}
+
+bool Card_isHovered(Card card, Vector2 point){
+    CardStr* c = (CardStr*)card;
+
+    c->hovered = CheckCollisionPointRec(point, c->rect);
+
+    return c->hovered;
+}
+
+bool Card_isGrabbed(Card card, bool isDown){
+    CardStr* c = (CardStr*)card;
+
+    if(c->grabbed){
+        c->grabbed = isDown;
+        c->attached = c->grabbed;
+    }
+    else c->grabbed = c->hovered && isDown;
+
+    return c->attached;
+}
+
+void Card_SetRotation(Card card, float rotation){
+    ((CardStr*)card)->rotation = rotation;
+}
+
+void Card_Move(Card card, Vector2 delta){
+    CardStr* c = (CardStr*)card;
+
+    c->rect.x += delta.x;
+    c->rect.y += delta.y;
+}
+
+void Card_MoveTo(Card card, Vector2 finalPoint, float duration){
+    CardStr* c = (CardStr*)card;
+
+    Vector2 fPoint;
+    fPoint.x = finalPoint.x - c->rect.width/2;
+    fPoint.y = finalPoint.y - c->rect.height/2;
+
+    Vector2 cPos = (Vector2){c->rect.x, c->rect.y};
+
+    Animation_SetPosition(c->anim, cPos);
+    Animation_MoveTo(c->anim, fPoint, duration);
+}
+
+void Card_Wiggle(Card card, float deltaTime){
+    CardStr* c = (CardStr*)card;
+
+    c->rotation = sin(c->time * 18) * PI/2;
+    c->time = c->time + deltaTime;
+}
+
+void Card_SetScaleRatio(Card card, float scaleRatio){
+    ((CardStr*)card)->scaleRatio = scaleRatio;
+}
+
+void Card_Minimize(Card card, float duration){
+    CardStr* c = (CardStr*)card;
+    
+    if(!c->isMaximized) return;
+    c->isMaximized = false;
+
+    if(Animation_ScaleIsAnimating(c->anim)) Animation_EndResize(c->anim);
+    c->rect = Animation_GetScale(c->anim);
+    Animation_Resize(c->anim, c->scaleRatio, duration);
+}
+
+void Card_Maximize(Card card, float duration){
+    CardStr* c = (CardStr*)card;
+
+    if(c->isMaximized) return;
+    c->isMaximized = true;
+
+    if(Animation_ScaleIsAnimating(c->anim)) Animation_EndResize(c->anim);
+    c->rect = Animation_GetScale(c->anim);
+    Animation_Resize(c->anim, 1.0f/c->scaleRatio, duration);
+}
+
+static void Card_UpdatePos(Card card, float deltaTime){
+    CardStr* c = (CardStr*)card;
+
+    Animation_UpdatePosition(c->anim, deltaTime);
+    
+    if(!Animation_PositionIsAnimating(c->anim)) return;
+    
+    Vector2 pos = Animation_GetPosition(c->anim);
+    c->rect.x = pos.x;
+    c->rect.y = pos.y;
+}
+
+static void Card_UpdateSize(Card card, float deltaTime){
+    CardStr* c = (CardStr*)card;
+
+    Animation_SetRectangle(c->anim, c->rect);
+    Animation_UpdateScale(c->anim, deltaTime);
+    
+    if(!Animation_ScaleIsAnimating(c->anim)) return;
+    
+    Rectangle scale = Animation_GetScale(c->anim);
+    c->rect = scale;
+    
+    printf("\n Scale = %.1f", scale.width);
+}
+
+void Card_Update(Card card, float deltaTime){
+    CardStr* c = (CardStr*)card;
+    
+    Card_UpdatePos(card, deltaTime);
+    Card_UpdateSize(card, deltaTime);
+
+    Vector2 point = GetMousePosition();
+    bool btnDown = IsMouseButtonDown(MOUSE_BUTTON_LEFT);
+
+    Card_isHovered(card, point);
+    Card_isGrabbed(card, btnDown);
+
+    if(c->attached) Card_Move(card, GetMouseDelta());
+
+    c->img.width = c->rect.width;
+    c->img.height = c->rect.height;
+}
+
+void Card_Draw(Card card){
+    CardStr* c = (CardStr*)card;
+
+    Rectangle source = (Rectangle){0, 0, c->rect.width, c->rect.height};
+    Rectangle dest = (Rectangle){
+        c->rect.x + c->img.width / 2.0f,
+        c->rect.y + c->img.height / 2.0f,
+        c->img.width,
+        c->img.height
+    };
+    Vector2 origin = (Vector2){c->img.width / 2.0f, c->img.height / 2.0f};
+
+    DrawTexturePro(c->img, source, dest, origin, c->rotation, WHITE);
+}
+
+Texture2D Card_free(Card card){
+    CardStr* c = (CardStr*)card;
+    Texture2D txr = c->img;
+    
+    free(c);
+
+    return txr;
+}
--- src/card.h
+++ src/card.h
@@ -6,30 +6,38 @@
 
 typedef void* Card;
 
-Card Card_Init(Rectangle rect, Texture2D path);
+/**
+ * @brief Inicializa uma estrutura de Card (carta).
+ * @param rect Retangulo de dimensoes para a carta.
+ * @param txr Textura ja' carregada na memo'ria para referenciacao na carta.
+ * @return Retorna um ponteiro para a estrutura de carta criada.
+ */
+Card Card_Init(Rectangle rect, Texture2D txr);
 
 Rectangle Card_GetRect(Card card);
 
-bool Card_isGrabbed(Card card);
+bool Card_isHovered(Card card, Vector2 point);
+
+bool Card_isGrabbed(Card card, bool isDown);
 
 void Card_SetRotation(Card card, float rotation);
 
-void Card_Move(Card card, bool minimizeOnGrab);
+void Card_Move(Card card, Vector2 delta);
 
-void Card_MoveTo(Card card, Vector2 finalPoint, float time);
+void Card_MoveTo(Card card, Vector2 finalPoint, float duration);
 
-void Card_Wiggle(Card card);
+void Card_Wiggle(Card card, float deltaTime);
 
-void Card_Minimize(Card card);
+void Card_SetScaleRatio(Card card, float scaleRatio);
 
-void Card_Maximize(Card card);
+void Card_Minimize(Card card, float duration);
 
-void Card_UpdateAnim(Card card, float deltaTime, bool centerAligned);
+void Card_Maximize(Card card, float duration);
 
-void Card_UpdateSize(Card card, float deltaTime);
+void Card_Update(Card card, float deltaTime);
 
 void Card_Draw(Card card);
 
-void Card_free();
+Texture2D Card_free(Card card);
 
 #endif
